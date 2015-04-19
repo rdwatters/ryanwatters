09fad1866a2ac83034e304c78c997d6372ecb66d
Ryan Watters <watters.ryan@gmail.com>
2015-04-19 01:03:25 -0500

    Changes to navigation and header

--- Roadmap.md
+++ Roadmap.md
@@ -6,9 +6,10 @@
 - [ ] Twitter Cards - https://dev.twitter.com/cards/overview
 - [ ] Schema.org for article pages - http://schema.org/Article
 - [ ] Google Plus Badges - https://developers.google.com/+/web/badge/
+- [ ] XML Sitemap
 - [ ] SEO-friendly alternative titles and descriptions in article content type
 
-###Search: 
+###Site Search: 
 
 - [ ] Swiftype Meta Tags - https://swiftype.com/documentation/meta_tags
 - [ ] Swiftype faceted search and custom styles - https://swiftype.com/documentation/tutorials/jquery
--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,2 +1,28 @@
 "use strict";
+
+window.onload = init;
+
+function init() {
+    var mobileButton = document.getElementById("toggle-mobile-navigation");
+    mobileButton.onclick = toggleMobileMenu;
+}
+
+function toggleMobileMenu() {
+    var mainMenu = document.getElementsByTagName("header")[0],
+        mainContent = document.querySelector(".main"),
+        theButton = document.getElementById("toggle-mobile-navigation"),
+        theFooter = document.getElementsByTagName("footer")[0];
+    if (mainMenu.classList.contains("menu-open")) {
+        console.log("Hello!");
+        mainMenu.classList.remove("menu-open");
+        mainContent.classList.remove("menu-open");
+        theFooter.classList.remove("menu-open");
+        theButton.classList.remove("menu-open");
+    } else {
+        mainMenu.classList.add("menu-open");
+        mainContent.classList.add("menu-open");
+        theFooter.classList.add("menu-open");
+        theButton.classList.add("menu-open");
+    }
+}
 //# sourceMappingURL=main.js.map
